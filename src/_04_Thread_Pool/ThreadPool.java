5dd21f6405e2c304a4f372ca5b96c4065ec29911
Nazo123 <48037675+Nazo123@users.noreply.github.com>
2021-03-25 18:33:46 -0700

    Learned About Synchronization

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,35 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
+	Thread[] threads;
+	ConcurrentLinkedQueue<Task> taskQue;
+
+	public ThreadPool(int amountOfThreads) {
+		threads = new Thread[amountOfThreads];
+		taskQue = new ConcurrentLinkedQueue<Task>();
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new Worker(taskQue));
+		}
+	}
+
+	public void addTask(Task object) {
+		taskQue.add(object);
+
+	}
+
+	public void start() {
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,12 +1,18 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
-
+ConcurrentLinkedQueue<Task> taskQue;
+	public Worker(ConcurrentLinkedQueue<Task> e) {
+		taskQue = e;
+	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		while(!taskQue.isEmpty()) {
+			taskQue.remove().preform();;
+		}
 	}
 
 
